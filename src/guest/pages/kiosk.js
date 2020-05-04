b0a57faf95ff303a0eb5c7c9ed98b4b1e717bed4
rejajamil <reja.copyright@gmail.com>
2020-05-04 20:06:34 +0700

    Up

--- src/guest/pages/kiosk.js
+++ src/guest/pages/kiosk.js
@@ -48,7 +48,7 @@ class KiosK extends React.Component {
         //     ))
         //   )
       }
-      , 1000 );
+      , 75 );
     });
     this.fetchData = setInterval(() => {
       axios.get(con.api+'/user/absen', {headers:con.headers, params:{device_id:this.state.selectedDevices}}).then(res => {
